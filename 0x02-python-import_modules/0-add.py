f3486ca1f4725e23bea74c6e6a7f3be603c0dfd0
SiziphiweD <siziphiwes13@gmail.com>
2023-01-08 11:26:40 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-def main():
+
     a = 1
     b = 2
+    """"Print the sum of 1 and 2.""""	
     from add_0 import add
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
 
 if __name__ == "__main__":
-    main()
