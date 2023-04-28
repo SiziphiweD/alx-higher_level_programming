3a5550d0234fb10d4bbe18ae0ead549a771721be
SiziphiweD <siziphiwes13@gmail.com>
2023-04-28 17:20:08 +0000

    javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-// a script that prints javaScript is amazing  using the 'node' intepreter
+// a script that prints “JavaScript is amazing” using the 'node' intepreter
 
-// This creates a constant variable called myVar and assigns the string value to it.
+// This creates a const variable called myVar and assigns the string value to it.
 const myVar = 'JavaScript is amazing';
 
 // console.log() function is used to print the value of myVar to the console.
 console.log(myVar);
-
